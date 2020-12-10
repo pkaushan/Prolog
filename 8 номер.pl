c90c0412dbc6e9ad2324a665f65221fe91ecab48
pkaushan <71364648+pkaushan@users.noreply.github.com>
2020-12-10 18:05:00 +0700

    Update 8 номер.pl

--- 8 номер.pl
+++ 8 номер.pl
@@ -4,17 +4,21 @@ deleteLast([], []).  %для пустого списка
 deleteLast([_], []).  %для списка с одним элементом
 deleteLast([X | Tail], [X | Tail2]) :-  %убираем голову списка
     deleteLast(Tail, Tail2). 
-copy(List, List).  %копируется список
-for(List, I, N, Res):- 
-I<N, I1 is I+1, 
+%copy(List, List). копируется список
+copy([],[]).		
+copy([H|List],[H|List]):- 
+    copy(List,List).
+for(List, I, N, Res):- %Предикат цикла, выполняется, пока I<N
+	I<N, 
+    I1 is I+1, 
     deleteLast(List, List1), 
     for(List1,I1,N,Res2), 
     copy(Res2, Res). 
-for(List, I,N, Res):- 
+for(List, I,N, Res):- %Предикат цикла, выполняется, пока I=N
     I = N, 
     copy(List, Res). 
-q(L, X, L1):-
-    for(L, 0, X, L1), !.
+q(L, X, L1):- %Искомый предикат
+    for(L, 0, X, L1).
 
 
 /** <examples>
